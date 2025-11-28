bb9bee21298ff1efd3cd148f793ba2896842aa60
JESSURI CRISTAL BAEZA CRUZ <cristal200479@gmail.com>
2025-11-28 00:39:54 -0700

    actualice las horas de horario ts

--- src/app/terapeuta/horarios/horarios.ts
+++ src/app/terapeuta/horarios/horarios.ts
@@ -20,7 +20,7 @@ export class HorariosComponent implements OnInit {
   mensajeAccion = signal<string | null>(null);
   advertencias = signal<string[]>([]);
 
-  horas = ['08:00','09:00','10:00','11:00','12:00','13:00','14:00','15:00','16:00','17:00','18:00','19:00','20:00'];
+  horas = ['08:00','09:00','10:00','11:00','12:00','13:00','14:00','15:00','16:00','17:00','18:00'];
   diasSemana = [
     { num: 1, label: 'Lun' },
     { num: 2, label: 'Mar' },
