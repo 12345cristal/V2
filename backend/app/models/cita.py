072ec0504ffe8d9e4200a9bbdcbab34bcc69ae47
JESSURI CRISTAL BAEZA CRUZ <cristal200479@gmail.com>
2025-12-04 20:29:00 -0700

    cree modulo citas coordinador

---
+++ backend/app/models/cita.py
@@ -0,0 +1,51 @@
+# app/models/cita.py
+from sqlalchemy import Column, Integer, String, Text, Date, Time, ForeignKey, Boolean
+from sqlalchemy.orm import relationship
+from app.db.base_class import Base
+
+
+class Cita(Base):
+    __tablename__ = "citas"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    nino_id = Column(Integer, ForeignKey("ninos.id", ondelete="SET NULL"))
+    es_nuevo_nino = Column(Boolean, default=False)
+
+    temp_nino_nombre = Column(String(100))
+    temp_nino_apellido_paterno = Column(String(50))
+    temp_nino_apellido_materno = Column(String(50))
+
+    temp_tutor_nombre = Column(String(100))
+    temp_tutor_apellido_paterno = Column(String(50))
+    temp_tutor_apellido_materno = Column(String(50))
+
+    telefono_temporal = Column(String(20))
+
+    terapeuta_id = Column(Integer, ForeignKey("personal.id", ondelete="SET NULL"))
+    terapia_id = Column(Integer, ForeignKey("terapias.id", ondelete="SET NULL"))
+
+    fecha = Column(Date, nullable=False)
+    hora_inicio = Column(Time, nullable=False)
+    hora_fin = Column(Time, nullable=False)
+
+    estado_id = Column(Integer, ForeignKey("cat_estado_cita.id", ondelete="RESTRICT"))
+    es_reposicion = Column(Boolean, default=False)
+
+    motivo = Column(Text)
+    diagnostico_presuntivo = Column(String(255))
+    observaciones = Column(Text)
+
+    observadores = relationship("CitaObservador", back_populates="cita")
+    nino = relationship("Nino")
+    terapeuta = relationship("Personal")
+    terapia = relationship("Terapia")
+
+class CitaObservador(Base):
+    __tablename__ = "citas_observadores"
+
+    id = Column(Integer, primary_key=True)
+    cita_id = Column(Integer, ForeignKey("citas.id", ondelete="CASCADE"))
+    terapeuta_id = Column(Integer, ForeignKey("personal.id", ondelete="CASCADE"))
+
+    cita = relationship("Cita", back_populates="observadores")
